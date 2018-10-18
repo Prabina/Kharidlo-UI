20fae98defad3d452a083be63de35e69598e73c4
Ajesh Nair <ajesh.nair16@gmail.com>
2018-10-18 17:43:13 +0530

    initial failing test for product list

--- app/src/test/java/com/ecommerce/kharidlo_ui/ExampleUnitTest.java
+++ app/src/test/java/com/ecommerce/kharidlo_ui/ExampleUnitTest.java
@@ -3,6 +3,7 @@ package com.ecommerce.kharidlo_ui;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +14,10 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void shouldReturnPoductListWhenSearchKeywordPassed(){
+        assertEquals(true,false);
     }
 }
