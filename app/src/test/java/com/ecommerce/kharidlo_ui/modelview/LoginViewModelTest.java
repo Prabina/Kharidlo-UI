fa41f0ba85d5c082225b94c18b3ead7e6b885290
tarakchand <tarakchand@gmail.com>
2018-10-22 13:08:28 +0530

    Implemented LoginViewModel

---
+++ app/src/test/java/com/ecommerce/kharidlo_ui/modelview/LoginViewModelTest.java
@@ -0,0 +1,36 @@
+package com.ecommerce.kharidlo_ui.modelview;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LoginViewModelTest {
+
+    @Test
+    public void shouldReturnSuccessfulLogin() {
+        LoginViewModel loginViewModel = mock(LoginViewModel.class);
+        String username = "android";
+        String password = "123456";
+        String testString = "Successfully Logged in.";
+
+
+        when(loginViewModel.login(username, password)).thenReturn(testString);
+
+        String loginMsg = loginViewModel.login(username, password);
+
+        assertThat(loginMsg, is(testString));
+
+//        verify(loginViewModel,times(1)).login(username,password);
+
+    }
+
+
+
+
+}
